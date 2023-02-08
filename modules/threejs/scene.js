db84908c607c6dcf4deef17ee9596eb3c2bdad32
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2023-02-08 16:46:44 +0100

    feat: add three mesh bvh

--- modules/threejs/scene.js
+++ modules/threejs/scene.js
@@ -3,8 +3,13 @@ import * as THREE from 'three';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
+import { computeBoundsTree, disposeBoundsTree, acceleratedRaycast } from 'three-mesh-bvh';
 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+THREE.BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
+THREE.BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
+THREE.Mesh.prototype.raycast = acceleratedRaycast;
 
 export let scene;
 export let camera;
@@ -34,11 +39,14 @@ export const startScene = () => {
     controls.enableDamping = true;
     const loader = new GLTFLoader();
     loader.load( './../../models/construction_site_big.glb', function ( gltf ) {
-
         const model = gltf.scene;
         scene.add( model );
+        scene.traverse(function(obj){
+            if(obj.type === 'Mesh'){
+                obj.geometry.computeBoundsTree();
+            }
+        });
         animate();
-
     }, undefined, function ( e ) {
         console.error( e );
     } );
